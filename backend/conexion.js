b639c708cbf7ceb4c8dab0ca148920cf676c980c
Mayte-hub <maytequinoneztrujillo@gmail.com>
2025-09-26 12:46:20 -0500

    Actualizar configuración y conexión

--- backend/conexion.js
+++ backend/conexion.js
@@ -1,17 +1,19 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const conexion = mysql.createConnection({
-  host: "localhost",
-  user: "root",       // usuario por defecto en XAMPP
-  password: "",       // si tienes contraseña ponla aquí
-  database: "inventario" // el nombre de tu BD
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 conexion.connect((err) => {
   if (err) {
     console.error("❌ Error al conectar a la base de datos:", err);
   } else {
-    console.log("✅ Conectado a MySQL - Base de datos: inventario");
+    console.log("✅ Conectado a MySQL - Base de datos:", process.env.DB_NAME);
   }
 });
 
